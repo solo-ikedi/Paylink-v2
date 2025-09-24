012da8f14704e1d993bc633bd3daa3c3b83a2b57
Ubani Solomon Ikedi <solozikedi@gmail.com>
2025-09-24 02:41:53 +0100

    Fix Vite base path for Netlify deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  
-  base: "./",
+  base: "./", // important for Netlify deploy
 });
